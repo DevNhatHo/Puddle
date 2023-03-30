186f0c1bb84251dabf9c36dcdc5d56a91b861069
Nhat Ho <sdnhatb@gmail.com>
2023-03-29 17:26:43 -0700

    added details page. shows item details

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Item
+
+# Create your views here. pull from database
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    return render(request, 'item/detail.html',{
+        'item': item
+
+    })
